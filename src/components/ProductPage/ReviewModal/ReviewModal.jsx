82e50e501246d3a1dfc01b8dd3d1ded1b1e166ff
OmarSalah95 <Omars.e.995@gmail.com>
2020-02-12 15:04:58 -0800

    Adds form inputs and form input handling, still crude moving needs styling

--- src/components/ProductPage/ReviewModal/ReviewModal.jsx
+++ src/components/ProductPage/ReviewModal/ReviewModal.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+
 export default function ReviewModal({submit, exit}){
+    const [newReview, updateReview] = useState({
+        subject:"",
+        poster:"",
+        review:"",
+        rating: 0,
+        created_at: ""
+    })
+
+    const handleInput = (event) => updateReview({...newReview, [event.target.name]: event.target.value})
+
+    console.log(newReview)
     return(
         <>
         <div className="filter-layer">
         </div>
         <div className="form-wrapper" >
             <h1>ADD REVIEW</h1>
-            <form className="review-form">
-
+            <form className="review-form" onSubmit={(newRev)=> submit(newRev)}>
+                <label>
+                    Rating
+                    <select>
+                        {Array(5).fill(1).map((one, i)=> <option value={one+1}>{one+i} Stars</option>)}
+                    </select>
+                </label>
+                <label>
+                    Review Title
+                    <input 
+                        name="subject"
+                        type="text"
+                        onChange={event => handleInput(event)}
+                    />
+                </label>
+                <label>
+                    Your Name
+                    <input 
+                        name="poster"
+                        type="text"
+                        onChange={event => handleInput(event)}
+                    />
+                </label>
+                <label>
+                    Write Your Review Below
+                    <textarea 
+                        name="review"
+                        type="text-field"
+                        onChange={event => handleInput(event)}
+                    />
+                </label>
             </form>
         </div>
         </>
